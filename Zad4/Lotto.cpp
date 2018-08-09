f8149e90b559dc275b6857a0c022bf2127736b7a
Kamil <kamil.mielnik@tieto.com>
2018-08-09 04:05:40 -0700

    Lotto(Task4) created project template.

---
+++ Zad4/Lotto.cpp
@@ -0,0 +1,7 @@
+#include <assert.h>
+#include "my_functions.c"
+
+int main()
+{
+    test_cases();
+}
---
+++ Zad4/my_functions.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+/** 
+* Function returns an array of 5 elements.
+* Those 5 elements are created randomly in the range 1 - 49.
+* Numbers can't repeat.
+*/
+int* Lotto_drawing()
+{
+	
+}
+
+/* Please create test cases for this program. test_cases() function can return void, bool or int. */
+void test_cases()
+{
+    assert(1==1);
+}
