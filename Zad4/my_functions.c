012f7db05ac204d06ae4f31cdbf56bdfaa2492ad
Kamil <kamil.mielnik@tieto.com>
2018-08-09 04:47:02 -0700

    Lotto(Task4) wrote test_cases.

--- Zad4/my_functions.c
+++ Zad4/my_functions.c
@@ -1,4 +1,7 @@
 #include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 /** 
 * Function returns an array of 5 elements.
 * Those 5 elements are created randomly in the range 1 - 49.
@@ -6,11 +9,34 @@
 */
 int* Lotto_drawing()
 {
-	
 }
 
 /* Please create test cases for this program. test_cases() function can return void, bool or int. */
 void test_cases()
 {
-    assert(1==1);
+    //check if all numbers are in range
+    int *numbers = Lotto_drawing();
+    bool any_not_in_range = false;
+    for(int i=0;i<5;i++)
+        if(numbers[i] < 1 || numbers[i] > 49)
+            any_not_in_range = true;
+    assert(any_not_in_range == false);
+
+    //check if numbers do not repeat themselves
+    numbers = Lotto_drawing();
+    bool any_repeated = false;
+    bool range[50];
+    for(int i=0;i<49;i++)
+        range[i] = false;
+    for(int i=0;i<5;i++)
+    {
+        int actual_number = numbers[i];
+        if(range[actual_number] == true)
+        {
+            any_repeated = true;
+            break;
+        }
+        range[actual_number] = true;
+    }
+    assert(any_repeated == false);
 }
