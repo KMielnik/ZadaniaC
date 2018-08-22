d47671d3ea825d630bf90cddd4e4517da6daf186
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-08-22 15:56:27 +0200

    IMDB API - opening trailers in youtube

--- imdb_api.py
+++ imdb_api.py
@@ -4,11 +4,8 @@ import os
 import mimetypes
 import re
 import urllib.request
-
-API_KEY = "4ed3fc46"
-omdb.set_default('apikey', API_KEY)
-
-running = True
+import urllib.parse
+import webbrowser
 
 
 def downloadTitlesFromFolder(folder: str):
@@ -37,30 +34,31 @@ def downloadTitlesFromFolder(folder: str):
     return titles[:-2]
 
 
-while running:
-    movies = []
-    moviesFolder = 'C:\Movies'
-    userInput = input(f"[W]pisujesz nazwy, czy mam je [P]obrać z '{moviesFolder}'? : ")
-    if userInput == 'W':
-        userInput = input("Podaj tytuły filmów: ")
-    elif userInput == 'P':
-        userInput = downloadTitlesFromFolder(moviesFolder)
-    else:
-        continue
+def printMoviesToConsole(movies):
+    for movie in movies:
+        print(f"Title: {movie['title']}")
+        print(f"\tRelease date: {movie['released']}")
+        movieDatetime = datetime.strptime(movie['released'], '%d %b %Y')
+        print(f"\tRelease date: {movieDatetime:%d.%m.%Y}")
+        print(f"\tRatings:")
+        for rating in movie['ratings']:
+            print(f"\t\t{rating['source']}: {rating['value']}")
+        print(f"\tPopularity(votes): {movie['imdb_votes']}")
+        print(f"\tLength: {movie['runtime']}")
+        print()
 
-    titleNames = userInput.split(' : ')[0]
-    try:
-        sortBy = userInput.split(' : ')[1]
-    except:
-        sortBy = ''
 
-    for title in titleNames.split(', '):
-        movie = omdb.get(title=title)
-        if len(movie) == 0:
-            print(f"Nie znaleziono {title} w bazie.")
-        else:
-            movies.append(movie)
+def createFoldersForMovies(movies, moviesFolder):
+    for movie in movies:
+        if not os.path.exists(f"{moviesFolder}/{movie['title']}"):
+            os.mkdir(f"{moviesFolder}/{movie['title']}")
+        movie_description = open(f"{moviesFolder}/{movie['title']}/opis.txt", 'w')
+        movie_description.write(str(movie))
+        movie_description.close()
+        urllib.request.urlretrieve(movie['poster'], f"{moviesFolder}/{movie['title']}/poster.jpg")
 
+
+def sortMovies(movies, sortBy):
     if sortBy == 'Rating':
         print(f"Sorting by {sortBy} from best rated")
         movies.sort(key=lambda x: (int(x['ratings'][1]['value'].replace('%', '')), x), reverse=False)
@@ -76,28 +74,61 @@ while running:
     if sortBy == 'Popularity':
         print(f"Sorting by {sortBy} from most voted")
         movies.sort(key=lambda x: (int(x['imdb_votes'].replace(',', '')), x), reverse=True)
+    return movies
 
+
+def createMoviesDatabase(userInput):
+    movies = []
+    titleNames = userInput.split(' : ')[0]
+    try:
+        sortBy = userInput.split(' : ')[1]
+    except:
+        sortBy = ''
+
+    for title in titleNames.split(', '):
+        movie = omdb.get(title=title)
+        if len(movie) == 0:
+            print(f"Nie znaleziono {title} w bazie.")
+        else:
+            movies.append(movie)
+    return movies, sortBy
+
+
+def openTrailers(movies):
     for movie in movies:
-        print(f"Title: {movie['title']}")
-        print(f"\tRelease date: {movie['released']}")
-        movieDatetime = datetime.strptime(movie['released'], '%d %b %Y')
-        print(f"\tRelease date: {movieDatetime:%d.%m.%Y}")
-        print(f"\tRatings:")
-        for rating in movie['ratings']:
-            print(f"\t\t{rating['source']}: {rating['value']}")
-        print(f"\tPopularity(votes): {movie['imdb_votes']}")
-        print(f"\tLength: {movie['runtime']}")
+        query_string = urllib.parse.urlencode({"search_query": movie['title'] + ' trailer'})
+        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+        webbrowser.open("http://www.youtube.com/watch?v=" + search_results[0])
+
+
+def main():
+    omdb.set_default('apikey', '4ed3fc46')
+
+    while True:
+        moviesFolder = 'C:\Movies'
+        userInput = input(f"[W]pisujesz nazwy, czy mam je [P]obrać z '{moviesFolder}'? : ")
+        if userInput == 'W':
+            userInput = input("Podaj tytuły filmów: ")
+        elif userInput == 'P':
+            userInput = downloadTitlesFromFolder(moviesFolder)
+        else:
+            continue
+
+        movies, sortBy = createMoviesDatabase(userInput)
+        sortMovies(movies, sortBy)
+        printMoviesToConsole(movies)
+        createFoldersForMovies(movies, moviesFolder)
+
+        userInput = input("Chcesz wyświetlić trailery? [T]ak : ")
+        if userInput == 'T':
+            openTrailers(movies)
+
+        userInput = input("Chcesz kontynuować? ('end' konczy program): ")
+        if userInput == 'end':
+            break
         print()
 
-    for movie in movies:
-        if not os.path.exists(f"{moviesFolder}/{movie['title']}"):
-            os.mkdir(f"{moviesFolder}/{movie['title']}")
-        movie_description = open(f"{moviesFolder}/{movie['title']}/opis.txt",'w')
-        movie_description.write(str(movie))
-        movie_description.close()
-        urllib.request.urlretrieve(movie['poster'],f"{moviesFolder}/{movie['title']}/poster.jpg")
 
-    userInput = input("Chcesz kontynuować? ('end' konczy program): ")
-    if userInput == 'end':
-        running = False
-    print()
+if __name__ == "__main__":
+    main()
